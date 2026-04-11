8e7c12be4360b7a916832daa5eb29d0423c70bba
Chebupell Aka <90975428+Ophlata@users.noreply.github.com>
2026-04-11 14:13:44 +0500

    mainCharacter and animator+gravity
    прыжок бег на шифт аниматор и графитация

--- SandEscapesUn/Assets/_projects/Scripts/PlayerController.cs
+++ SandEscapesUn/Assets/_projects/Scripts/PlayerController.cs
@@ -1,13 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5.0f; // Скорость движения персонажа
+    public float walkSpeed = 5f;
+    public float runSpeed = 9f;
+    public float gravity = -20f;
+    public float jumpHeight = 2f;
+
+    public float coyoteTime = 0.15f;
+    private float coyoteTimer;
 
     private CharacterController controller;
-    
+    private Vector3 velocity;
+
     private void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -17,17 +22,37 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // Получаем ввод от игрока
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        
-        // Вычисляем направление движения
-        Vector3 moveDirection = transform.forward * verticalInput + transform.right * horizontalInput;
-        
-        // Применяем гравитацию
-        moveDirection.y -= 9.81f * Time.deltaTime;
-        
-        // Двигаем персонажа
-        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // вот это важно
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (controller.isGrounded)
+        {
+            coyoteTimer = coyoteTime;
+
+            if (velocity.y < 0)
+                velocity.y = -2f;
+        }
+        else
+        {
+            coyoteTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && coyoteTimer > 0f)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            coyoteTimer = 0f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
